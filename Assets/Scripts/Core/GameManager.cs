fd419fa38006bbcd2217537b9e235f4220e0e46d
Lucas Gariador <lucas_manu20@outlook.com>
2024-11-23 00:14:17 -0300

    Basic Ia and MonoSingleton

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    [SerializeField]
+    private Transform playerPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
